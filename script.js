ca874b4eb3f4bc7f819af32fe929b9a669825ad1
PHSS10 <pedrohenriquesousas14@gmail.com>
2024-02-23 21:45:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 /* Desenvolva seu código abaixo */
+function playRockPaperScissor(player1, player2){
+    if(player1 == "Papel" && player2 == "Pedra" || player1 == "Tesoura" && player2 == "Papel" || player1 == "Pedra" && player2 == "Tesoura") {
+        return "Jogador 1 venceu!"
+    } else if (player2 == "Papel" && player1 == "Pedra" || player2 == "Tesoura" && player1 == "Papel" || player2 == "Pedra" && player1 == "Tesoura"){
+        return "Jogador 2 venceu!"
+    } else {
+        return "Empate!"
+    }
+    }
+console.log(playRockPaperScissor("Pedra","Papel"));
